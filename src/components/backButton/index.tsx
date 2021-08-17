93621c85de78be8d4d376cafb7a0902cd6f899ab
Almeida <victor.freitas08@hotmail.com>
2021-08-17 19:03:35 -0300

    navigation go back

--- src/components/backButton/index.tsx
+++ src/components/backButton/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.css";
 export default function BackButton(props: any) {
   return (
-    <button className="back-button" type="submit">
+    <button className="back-button" onClick={props.onClick}>
       <label>{props.text}</label>
     </button>
   );
--- src/pages/createUser/index.tsx
+++ src/pages/createUser/index.tsx
@@ -3,8 +3,14 @@ import "./styles.css";
 import Input from "../../components/input";
 import SubmitButton from "../../components/submitButton";
 import BackButton from "../../components/backButton";
+import { useHistory } from "react-router";
 
 export default function CreateUser() {
+  const history = useHistory();
+  const handleClick = (e:any) => {
+    e.preventDefault();
+    history.goBack();
+  };
   return (
     <div className="container">
       <div className="input-container">
@@ -13,7 +19,7 @@ export default function CreateUser() {
         <Input label="Password" />
         <Input label="Confirm password" />
         <div className="button-container">
-          <BackButton text="Back" />
+          <BackButton text="Back" onClick={handleClick} />
           <SubmitButton text="Create" />
         </div>
       </div>
