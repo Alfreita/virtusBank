edee5424960af5c451fe5005da20564589599243
Almeida <victor.freitas08@hotmail.com>
2021-08-17 17:41:44 -0300

    adding submit component

---
+++ src/components/submitButton/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.css";
+export default function SubmitButton() {
+  return (
+    <button className="button" type="submit">
+      Login
+    </button>
+  );
+}
