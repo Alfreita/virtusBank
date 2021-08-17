59f27258b07f09f7567e90841055326a88bd242a
Almeida <victor.freitas08@hotmail.com>
2021-08-16 22:13:38 -0300

    login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { useHistory } from "react-router";
+import "./styles.css";
 
 export default function Login() {
+  const history = useHistory();
+  const handleLogin = (e: any) => {
+    e.preventDefault();
+    history.push("/profile");
+  };
   return (
-    <div>
-      <h1>login</h1>
-    </div>
+    <form onSubmit={handleLogin}>
+      <div className="container">
+        <div className="logon-container">
+          <div className="fields-container">
+            <p>User:</p>
+            <input className="input" />
+          </div>
+          <div className="fields-container">
+            <p>Password:</p>
+            <input className="input" />
+          </div>
+          <button className="button" type="submit">
+            Login
+          </button>
+        </div>
+      </div>
+    </form>
   );
 }
