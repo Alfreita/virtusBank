b2d7c254c6a422f9e40eaf14cd14fb81780f4630
Almeida <victor.freitas08@hotmail.com>
2021-08-19 20:34:49 -0300

    tests in button component

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -1,9 +1,37 @@
 import { render, screen } from "@testing-library/react";
-import BackButton from ".";
+import userEvent from "@testing-library/user-event";
+import Button from ".";
 
-test("should exist back button", () => {
-  render(<BackButton />);
-  const backButton = screen.getByRole("button");
-  console.log(backButton);
-  expect(backButton).toBeInTheDocument();
+describe("Should test button component", () => {
+  test("should exist back button", () => {
+    render(<Button />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
+  test("should exist back-button style class", () => {
+    render(<Button type="button" className="back-button" />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass("back-button");
+  });
+
+  test("should exist submit-button style class", () => {
+    render(<Button type="submit" className="submit-button" />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass("submit-button");
+  });
+
+  test("should test click event in button", () => {
+    let click = false;
+    const submit = () => {
+      click = true;
+    };
+    render(<Button type="submit" className="submit-button" onClick={submit} />);
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass("submit-button");
+    expect(click).toBe(true);
+  });
 });
