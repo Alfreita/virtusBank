e455f6b80c70150395b9501f7fbdef589f506617
Victor Almeida <victorf@ciandt.com>
2021-08-19 14:59:41 -0300

    styling credit card

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -22,22 +22,25 @@ export default function Profile() {
       </div>
 
       <div className="card-container">
-        <Card
-          name="John Smith"
-          number="**** **** **** 1111"
-          expiry="10/20"
-          cvc="737"
-          preview={true}
-          issuer="mastercard"
-        />
-        <div>
-          <div>
-            <label>Limite:</label>
-            <label>$35.000</label>
+        <div className="card">
+          <Card
+            name="John Smith"
+            number="**** **** **** 1111"
+            expiry="10/20"
+            cvc="737"
+            preview={true}
+            issuer="mastercard"
+          />
+        </div>
+
+        <div className="card-limit">
+          <div className="card-label">
+            <label className="lblName">Limite:</label>
+            <label className="lblLimitTotal">$35.000</label>
           </div>
-          <div>
-            <label>Limite Utilizado:</label>
-            <label>$27.000</label>
+          <div className="card-label">
+            <label className="lblName">Utilizado:</label>
+            <label className="lblLimitUsed">$27.000</label>
           </div>
         </div>
       </div>
