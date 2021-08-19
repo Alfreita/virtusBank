e77112d318b59afeb05d2b1914ea7082e6e4100a
Victor Almeida <victorf@ciandt.com>
2021-08-19 14:13:41 -0300

    adding card in profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Brand from "../../components/brand";
 import "./styles.css";
+import Card from "react-credit-cards";
+import "react-credit-cards/es/styles-compiled.css";
 
 export default function Profile() {
   return (
@@ -16,6 +18,27 @@ export default function Profile() {
         </div>
         <div className="profileWidth">
           <label>Profile</label>
+        </div>
+      </div>
+
+      <div className="card-container">
+        <Card
+          name="John Smith"
+          number="**** **** **** 1111"
+          expiry="10/20"
+          cvc="737"
+          preview={true}
+          issuer="mastercard"
+        />
+        <div>
+          <div>
+            <label>Limite:</label>
+            <label>$35.000</label>
+          </div>
+          <div>
+            <label>Limite Utilizado:</label>
+            <label>$27.000</label>
+          </div>
         </div>
       </div>
     </div>
